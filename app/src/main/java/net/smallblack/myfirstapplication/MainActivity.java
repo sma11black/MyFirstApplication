23e57d8378edc101ed227889f7d4a1acd753a6e7
smallblack <653014496@qq.com>
2017-08-17 00:14:33 +0800

    Supporting Different Platform Versions

--- app/src/main/java/net/smallblack/myfirstapplication/MainActivity.java
+++ app/src/main/java/net/smallblack/myfirstapplication/MainActivity.java
@@ -1,17 +1,29 @@
 package net.smallblack.myfirstapplication;
 
+import android.app.ActionBar;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.main);
+
+        // Get a string resource from your app's Resources
+        String hello = getResources().getString(R.string.hello_world);
+
+        // Or supply a string resource to a method that requires a string
+        TextView textView = new TextView(this);
+        textView.setText(R.string.hello_world);
+
     }
 
     /** Called when the user clicks the Send button **/
@@ -23,5 +35,12 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
 
+    }
+    private void setUpActionBar() {
+        // Make sure we're running on Honeycomb or higher to use ActionBar APIs
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            ActionBar actionBar = getActionBar();
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
 }
